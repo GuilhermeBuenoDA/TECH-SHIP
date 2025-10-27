c3ef4095394798d0ed4b3c08a69e4e99b6b4d56e
Ryan Matos da Silva <ryan.28.ms@gmail.com>
2025-10-27 06:57:33 -0300

    novo tema

--- script.js
+++ script.js
@@ -8,3 +8,31 @@ function Comofunciona(){
         });
     }
 }   
+
+const pagina = document.getElementById("pagina");
+const botaoTrocarTema = document.getElementById("trocarTema");
+
+function trocarTema() {
+    if (pagina.classList.contains('tema-dark')) {
+        pagina.classList.remove('tema-dark');
+        botaoTrocarTema.textContent = "Alterar Para Tema Escuro";
+        localStorage.setItem('tema', 'claro');
+    }else {
+        pagina.classList.add("tema-dark");
+        botaoTrocarTema.textContent = "Alterar Para Tema Claro";
+        localStorage.setItem('tema', 'escuro');
+    }
+}
+botaoTrocarTema.addEventListener('click', trocarTema);
+
+(function carregarTema() {
+    const temaSalvo = localStorage.getItem('tema'); 
+
+    if (temaSalvo === 'dark') {
+        pagina.classList.add('tema-dark');
+        botaoTrocarTema.textContent = "Alterar Para Tema Claro";
+    } else {
+        pagina.classList.remove('tema-dark');
+        botaoTrocarTema.textContent = "Alterar Para Tema Escuro";
+    }
+})();
